30b8e0b50033d84891e2e7184adbdb16c48f5572
TheraBru <theresebruzell@gmail.com>
2026-02-09 14:15:14 +0100

    remove redundant code, fix bugs and add README

---
+++ README.md
@@ -0,0 +1,25 @@
+# :shipit: How to get started
+
+1. Clone the repository to your local machine.
+2. Withing the root folder run `npm i` to install dependencies. Make sure you have node and angular installed.
+3. Run application with command `ng serve`.
+
+## :eyes: Improvement suggestions and discussion
+
+- There are a few things I would have wanted to improve before release. One major part is connected to accessibility. As of now I added no ARIA to the countdown so a screenreader will not alert user of changes. The reason why I didn't add this change is because it is better to not use ARIA at all than to use it incorrectly and when it comes to countdowns it is a tricky line to walk. You want the user to be aware of the ticking no matter if they have full vision or not, but you also don't want to disrupt their flow by alerting continously. Had this been an actual product it would have been a good idea to experiment a bit to find the best ARIA behavior.
+
+- The countdown has some kinks that would have been good to handle before a release. A decision would have to be made for what behavior is expected if the user put in a date in the past for example.
+
+- The solution with the scaling heading works for most inputs, especially the kinds one might expect from an actual user. But there are a few edge cases where the text will either be smaller or wrap. This is because the solution is dependant on the average width of a letter. Some letters however are smaller (like i and l) and some letters are wider (like m and w) this means that inputs that contains an unnatural amount of either of these letters will cause a breaking behavior. One might suggest either living with this issue (it is after all an extreme edge case) or taking the type of characters into account in the calculation as well.
+
+- A decision that was made in the creation of this app was to add the quote for wider (and landscape) screens even though the Figma design seemed to count on it being absent. Had these been a real product I would have brought forward the question why this difference between the designs existed. For this assignment I took for granted that it was not on purpose and opted to keep the quote. This was because by WCAG and EN 301 549 standard all information on an application needs to be present through all screen sizes.
+
+- Before a launch I would also suggest that some unit and e2e tests were added, to make sure everything works as expected and will continue to do so.
+
+- Had this project been a real one and a part of a bigger structure then I would probably suggest using another structure of the directories as well as splitting up form and quote into different components.
+
+- This app is rendered on Client side which works fine for such a small and non-complex build. Had we added bigger database communication and larger algoritms it would probably be a better idea to render it server side.
+
+- I would also opt to use another API for the quotes, preferably one that is not third party or at the very least I can control the contents of. The one used for this project contained a few quite troubling and outright threatening and/or offensive quotes that might hurt the brand if a user would be met by it.
+
+- A last disclaimer is that I am a React and Vue developer. This was my first time building in Angular. Meaning I had to understand the framework before I could build the app. Had this been a proper product that was meant for lauch I would probably spend a bit more time on making sure the Angular syntax was up to standard.
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import quoteFetcher from './quoteFetcher';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { from } from 'rxjs';
+import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import quoteFetcher from './quoteFetcher'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { from } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -10,93 +10,93 @@ import { from } from 'rxjs';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-
 export class App {
-  title = signal('');
-  countdown = signal({ seconds: 0, minutes: 0, hours: 0, days: 0 });
-  quote = toSignal(from(quoteFetcher()), { initialValue: null });
-  selectedDate = signal<Date | null>(null);
+  title = signal('')
+  countdown = signal({ seconds: 0, minutes: 0, hours: 0, days: 0 })
+  quote = toSignal(from(quoteFetcher()), { initialValue: null })
+  selectedDate = signal<Date | null>(null)
 
-  private intervalId?: number;
-  private readonly STORAGE_TITLE = 'nc_title';
-  private readonly STORAGE_DATE = 'nc_selectedDate';
+  private intervalId?: number
+  private readonly STORAGE_TITLE = 'nc_title'
+  private readonly STORAGE_DATE = 'nc_selectedDate'
 
-  // TODO: Fix bug where picking a new date updates countdown to day prior
   millisecondsToFormat(ms: number) {
-    let seconds = ms / 1000;
+    let seconds = ms / 1000
 
-    const days = Math.trunc(seconds / 86400);
-    seconds = seconds % 86400;
+    const days = Math.trunc(seconds / 86400)
+    seconds = seconds % 86400
 
-    const hours = Math.trunc(seconds / 3600);
-    seconds = seconds % 3600;
+    const hours = Math.trunc(seconds / 3600)
+    seconds = seconds % 3600
 
-    const minutes = Math.trunc(seconds / 60);
-    seconds = seconds % 60;
+    const minutes = Math.trunc(seconds / 60)
+    seconds = seconds % 60
 
-    return { 
+    return {
       days: days,
-      hours: hours, 
-      minutes: minutes, 
-      seconds: Math.trunc(seconds) 
-    };
+      hours: hours,
+      minutes: minutes,
+      seconds: Math.trunc(seconds),
+    }
   }
 
   constructor() {
-    // restore persisted values
     try {
-      const savedTitle = localStorage.getItem(this.STORAGE_TITLE);
-      if (savedTitle) this.title.set(savedTitle);
+      const savedTitle = localStorage.getItem(this.STORAGE_TITLE)
+      if (savedTitle) this.title.set(savedTitle)
 
-      const savedDate = localStorage.getItem(this.STORAGE_DATE);
+      const savedDate = localStorage.getItem(this.STORAGE_DATE)
       if (savedDate) {
-        const parsed = new Date(savedDate);
-        if (!isNaN(parsed.getTime())) this.selectedDate.set(parsed);
+        const parsed = new Date(savedDate)
+        if (!isNaN(parsed.getTime())) this.selectedDate.set(parsed)
       }
-    } catch (e) {
-      // ignore localStorage errors (e.g., privacy mode)
-    }
+    } catch (e) {}
+
     this.intervalId = window.setInterval(() => {
-      const selection = this.selectedDate();
+      const selection = this.selectedDate()
 
       if (selection) {
-        const now = new Date();
-        let diff = selection.getTime() - now.getTime();
-        
-        if (diff < 0) diff = 0;
-        this.countdown.set(this.millisecondsToFormat(diff));
+        const now = new Date()
+        let diff = selection.getTime() - now.getTime()
+
+        if (diff < 0) diff = 0
+        this.countdown.set(this.millisecondsToFormat(diff))
       }
-    }, 1000);
+    }, 1000)
   }
 
   onTitleChange(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    this.title.set(inputElement.value);
-    try { localStorage.setItem(this.STORAGE_TITLE, inputElement.value); } catch {}
+    const inputElement = event.target as HTMLInputElement
+    this.title.set(inputElement.value)
+    try {
+      localStorage.setItem(this.STORAGE_TITLE, inputElement.value)
+    } catch {}
   }
 
   onDateChange(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    const currentDate = new Date();
-    const selected = new Date(inputElement.value + 'T00:00:00');
-    
-    this.selectedDate.set(selected);
-    try { localStorage.setItem(this.STORAGE_DATE, selected.toISOString()); } catch {}
-    
-    const timeDifference = selected.getTime() - currentDate.getTime();
-    const formattedTimeDifference = this.millisecondsToFormat(timeDifference);
-    
-    this.countdown.set(formattedTimeDifference);
+    const inputElement = event.target as HTMLInputElement
+
+    const currentDate = new Date()
+    const selected = new Date(inputElement.value)
+    this.selectedDate.set(selected)
+    try {
+      localStorage.setItem(this.STORAGE_DATE, selected.toISOString())
+    } catch {}
+
+    const timeDifference = selected.getTime() - currentDate.getTime()
+    const formattedTimeDifference = this.millisecondsToFormat(timeDifference)
+
+    this.countdown.set(formattedTimeDifference)
   }
 
   get selectedDateIso(): string {
-    const sd = this.selectedDate();
-    return sd ? sd.toISOString().slice(0,10) : '';
+    const sd = this.selectedDate()
+    return sd ? sd.toISOString().slice(0, 10) : ''
   }
 
   ngOnDestroy(): void {
     if (this.intervalId) {
-      clearInterval(this.intervalId);
+      clearInterval(this.intervalId)
     }
   }
 }
